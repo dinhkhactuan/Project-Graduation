d8c174977039a9d5621db42474698d49c295c750
Tuan <dinhkhactuan2002@gmail.com>
2024-08-18 21:21:10 +0700

    Update Ui

--- src/shared/home/itemsMenu.model.tsx
+++ src/shared/home/itemsMenu.model.tsx
@@ -22,6 +22,9 @@ export function useMenuItems(): MenuItem[] {
     {
       label: "Thế giới",
       key: "3",
+      onClick: () => {
+        router.push("/world");
+      },
       children: [
         {
           label: "Option 4",
@@ -36,22 +39,37 @@ export function useMenuItems(): MenuItem[] {
     {
       key: "4",
       label: "Kinh doanh",
+      onClick: () => {
+        router.push("/business");
+      },
     },
     {
       key: "5",
       label: "Start up",
+      onClick: () => {
+        router.push("/start-up");
+      },
     },
     {
       key: "6",
       label: "Giải trí",
+      onClick: () => {
+        router.push("/entertainment");
+      },
     },
     {
       key: "7",
       label: "Thể thao",
+      onClick: () => {
+        router.push("/sport");
+      },
     },
     {
       key: "8",
       label: "Tin mới nhất",
+      onClick: () => {
+        router.push("/news");
+      },
     },
     {
       key: "9",
@@ -64,23 +82,38 @@ export function useMenuItems(): MenuItem[] {
     {
       key: "11",
       label: "Du lịch",
+      onClick: () => {
+        router.push("/toursm");
+      },
     },
 
     {
       key: "12",
       label: "Đời sống",
+      onClick: () => {
+        router.push("/life");
+      },
     },
     {
       key: "13",
       label: "Khoa học",
+      onClick: () => {
+        router.push("/science");
+      },
     },
     {
       key: "14",
       label: "Giáo dục",
+      onClick: () => {
+        router.push("/education");
+      },
     },
     {
       key: "15",
       label: "Tâm sự",
+      onClick: () => {
+        router.push("/confide");
+      },
     },
   ];
 }
