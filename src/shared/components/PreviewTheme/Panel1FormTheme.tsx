31133c7134d18df6ad0ec0aaf5534de586098c99
Dinh Tuan <dinhkhactuan2002@gmail.com>
2024-08-19 19:48:13 +0700

    revmove var nerver use

--- src/shared/components/PreviewTheme/Panel1FormTheme.tsx
+++ src/shared/components/PreviewTheme/Panel1FormTheme.tsx
@@ -16,7 +16,7 @@ const Themes: { [key in Theme_Option]: Theme } = {
   [Theme_Option.THEME4]: THEME_DATA_THREE,
 };
 const Panel1FormTheme = () => {
-  const { themeData, setCurrentTheme } = useTheme();
+  const { setCurrentTheme } = useTheme();
 
   const [selectedLayout, setSelectedLayout] = useState(Theme_Option.THEME1);
 
