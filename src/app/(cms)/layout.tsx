9083bb989ec2013c5f599574d0d18eaeb8a0725f
Dinh Tuan <dinhkhactuan2002@gmail.com>
2024-09-01 01:31:26 +0700

    Ui dashboad

--- src/app/(cms)/layout.tsx
+++ src/app/(cms)/layout.tsx
@@ -1,5 +1,6 @@
+import { ReactElement } from "react";
 import AuthProtected from "../(provider)/AuthProtected";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: { children: ReactElement }) {
   return <AuthProtected>{children}</AuthProtected>;
 }
