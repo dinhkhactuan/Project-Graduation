4d458834cc8fbc6f60312a8684dc9a9f3e3e9626
Dinh Tuan <dinhkhactuan2002@gmail.com>
2024-09-09 00:55:15 +0700

    update rss

--- src/app/(lang)/start-up/page.tsx
+++ src/app/(lang)/start-up/page.tsx
@@ -3,7 +3,7 @@ import NewsEvent from "../news/newsEvent/NewsEvent";
 import { TypePage } from "@/service/TypePage";
 
 const PageNews = async () => {
-  const rawResult = await LatestNews("tin-noi-bat.rss");
+  const rawResult = await LatestNews();
 
   return (
     <>
